b5359ad7025fb0d0287456e0f36f85074ff03d32
NaaRaychel <Rachelnaayemofio@gmail.com>
2023-04-21 21:59:13 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
-
 /**
- * main - prints all combinations of single digis
+ * main - Entry point
+ *
  * Return: Always 0
  */
+
 int main(void)
 {
-	int n;
+	int a;
 
-	for (n = 48; n <= 57; n++)
+	for (a = 0; a < 10; a++)
 	{
-		putchar(n);
-		putchar(',');
-		putchar(' ');
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
-		return (0);
+	return (0);
 }

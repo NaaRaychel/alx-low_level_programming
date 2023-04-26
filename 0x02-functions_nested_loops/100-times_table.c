ac181744f2d57f19bcd56116ebe13800c2822d04
NaaRaychel <Rachelnaayemofio@gmail.com>
2023-04-25 22:39:05 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,7 @@ void print_times_table(int n)
 	{
 		for (num = 0; num <= n; num++)
 		{
-			putchar('0');
+			_putchar(48);
 
 			for (mult = 1; mult <= n; mult++)
 			{
@@ -28,14 +28,14 @@ void print_times_table(int n)
 
 				if (prod >= 100)
 				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
 				}
 				else if (prod <= 99 && prod >= 10)
 				{
-					_putchar((prod / 10) + '0');
+					_putchar((prod / 10) + 48);
 				}
-				_putchar((prod % 10) + '0');
+				_putchar((prod % 10) + 48);
 			}
 			_putchar('\n');
 		}
